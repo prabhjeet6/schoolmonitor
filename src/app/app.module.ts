ebe245c2b7450e19473ed3fc75fd749f678c136a
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2019-04-13 12:38:25 +0530

    changes for css from here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { FormsModule } from '@angular/forms'; 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent
+    
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
