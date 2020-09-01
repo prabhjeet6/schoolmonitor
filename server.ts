1f885bd3cd35d085afa04229261f896358929bb1
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2020-09-01 19:03:19 +0530

    i18n Finally complete :)

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
 import {enableProdMode} from '@angular/core';
 // Express Engine
 import {ngExpressEngine} from '@nguniversal/express-engine';
@@ -17,7 +18,7 @@ enableProdMode();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
@@ -31,24 +32,29 @@ app.engine('html', ngExpressEngine({
 }));
 
 app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
+app.set('views', join(DIST_FOLDER, 'browser'));
 
-// Example Express Rest API endpoints
-// app.get('/api/**', (req, res) => { });
-// Serve static files from /browser
-app.get('*.*', express.static(DIST_FOLDER, {
+
+// Server static files from /browser
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
   maxAge: '1y'
 }));
 
 // All regular routes use the Universal engine
 app.get('*', (req, res) => {
-  res.render('index', { req });
+//this is for i18n
+  const supportedLocales = ['en', 'pa'];
+  const defaultLocale = 'en';
+  const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
 
-  
+  //check if the requested url has a correct format '/locale' and matches any of the supportedLocales
+  const locale = (matches && supportedLocales.indexOf(matches[1]) !== -1) ? matches[1] : defaultLocale;
 
+  res.render(`${locale}/index`, { req });
 });
 
 // Start up the Node server
 app.listen(PORT, () => {
   console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
+
