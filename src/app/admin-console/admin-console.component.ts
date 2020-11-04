89b5b7085f968d02023e03f79728e25a2ab11556
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2020-11-04 06:44:56 +0530

    fix for file upload..documentation to be added as comments in source code after understanding the area of  context

--- src/app/admin-console/admin-console.component.ts
+++ src/app/admin-console/admin-console.component.ts
@@ -4,10 +4,10 @@ import { Subscription } from 'rxjs/internal/Subscription';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 
-var httpOptions = {
+/*var httpOptions = {
   headers: new HttpHeaders({ 'Authorization':  localStorage.getItem('userToken'),'Accept':'multipart/form-data' })
   , responseType: 'blob'
-};
+};*/
 @Component({
   selector: 'app-admin-console',
   templateUrl: './admin-console.component.html',
@@ -24,18 +24,19 @@ uploadStatus:boolean|undefined;
 
   onUpload(data,form) {
     var formData = new FormData();
-    if(null!=data){
+
+     console.log("header value "+localStorage.getItem('userToken'))
+     console.log(JSON.stringify(data.files[0])  +"data.files is here");
+    
     formData.append('studentDataFile', data.files[0], data.files[0].name);
     
-    this.subscription=this.adminConsoleService.onUpload(formData).subscribe(x => {this.uploadStatus = true;/*setTimeout(()=> { this.uploadStatus = true;},2000); */ }, err => { this.uploadStatus = false; }, () => {this.subscription.unsubscribe});
-    form.clear();  
-  }
-
+    console.log(JSON.stringify(data)  +"data is here" );
     
-  }
+    if(null!=formData && null!=formData.entries()){
+    this.subscription=this.adminConsoleService.onUpload(formData).subscribe(x => {/*this.uploadStatus = true;*/console.log(x);setTimeout(()=> { this.uploadStatus = true;},2000);  }, err => { console.log(err);this.uploadStatus = false; }, () => {this.subscription.unsubscribe});
+    form.clear();  
+    }
+      
+  }   
+}  
 
-
-
-
-
-}
--- src/app/service/admin-console.service.ts
+++ src/app/service/admin-console.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
-const httpOptions = {
-  headers: new HttpHeaders({  'Authorization': localStorage.getItem('userToken') })
-  ,reportProgress: true,
-   
-};
 @Injectable({
   providedIn: 'root'
 })
 export class AdminConsoleService {
-  fileUploadUrl: string = 'http://localhost:8088/schoolmonitor/studentDataUpload';
   
+  fileUploadUrl: string = 'http://localhost:8088/schoolmonitor/studentDataUpload';
   
   constructor(private http: HttpClient) { }
   
   onUpload( data:FormData):Observable<any> {
-    
-    return this.http.post<any>(this.fileUploadUrl, data, httpOptions);
+    return this.http.post(this.fileUploadUrl, data,{
+      headers:  new HttpHeaders({ 'Authorization':  localStorage.getItem('userToken') }),
+      responseType: 'json' 
       
+    });
   }
 }
