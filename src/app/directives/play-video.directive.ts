3780df397af0f9d535cf9250910c69e8efda15f1
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2021-03-29 16:32:00 +0530

    corrected footer overlap, centered video and autoplay

--- src/app/directives/play-video.directive.ts
+++ src/app/directives/play-video.directive.ts
@@ -1,36 +1,44 @@
-import { Input,Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
+import { Input, Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 
 @Directive({
   selector: '[appPlayVideo]'
 })
 export class PlayVideoDirective {
-  
+
   @Input() clickedVideo: string;
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('click') onClick() {
     this.renderer.removeChild(
-        this.elementRef.nativeElement.
+      this.elementRef.nativeElement.
         parentElement.parentElement.
         parentElement.parentElement.
         parentElement, this.elementRef.nativeElement.
-        parentElement.parentElement.
-        parentElement.parentElement.parentElement.children[4]);
-    
-    var videoPlayerElement=document.createElement('div');
-    videoPlayerElement.innerHTML = '<iframe width="420" height="315" src='+this.clickedVideo+'></iframe>';
+          parentElement.parentElement.
+          parentElement.parentElement.parentElement.children[4]);
+
+    var videoPlayerElement = document.createElement('div');
+    //videoPlayerElement.innerHTML = '<iframe width="420" height="315" src='+this.clickedVideo+'></iframe>';
+
+    videoPlayerElement.style.textAlign = 'center';
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute("width", "420");
+    iframe.setAttribute("height", "315");
+    iframe.setAttribute("src", this.clickedVideo);
+    iframe.setAttribute ("allow","autoplay ;");   
+
+    videoPlayerElement.appendChild(iframe);
 
     this.renderer.insertBefore(this.elementRef.nativeElement.
       parentElement.parentElement.
       parentElement.parentElement.
-      parentElement,videoPlayerElement,
+      parentElement, videoPlayerElement,
       this.elementRef.nativeElement.
-      parentElement.parentElement.
-      parentElement.parentElement.
-      parentElement.children[4]);
+        parentElement.parentElement.
+        parentElement.parentElement.
+        parentElement.children[4]);
 
-      videoPlayerElement.click();
 
 
   }
