24e6bb0b03c81bb1045df476ad1cfd3475337a34
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2021-05-24 04:47:59 +0530

    mic button positioned as expected

--- src/app/online-coursework/online-coursework.component.ts
+++ src/app/online-coursework/online-coursework.component.ts
@@ -16,32 +16,36 @@ export class OnlineCourseworkComponent implements OnInit {
   subscriptionObject: Subscription;
   resultsReturned: boolean;
   searchTerm?: string;
-  results:any; 
-  
-  page?:number=1;
-   
+  results: any;
+
+  page?: number = 1;
+
   ngOnInit() {
-    if(this.page===undefined)
-      this.page=1;
-      if(this.result===undefined)  
-      this.result={};
+    if (this.page === undefined)
+      this.page = 1;
+    if (this.result === undefined)
+      this.result = {};
   }
-  search() { 
-    if(this.searchTerm){ 
-    this.subscriptionObject = this.onlineCourseworkService.search(this.searchTerm,this.page).subscribe(
-      x => { setTimeout(() => { this.result = x; this.postSearch(); }, 2000); }, err => { }, () => this.subscriptionObject.unsubscribe);
+  convertSpeechToText(){
+    //TODO
+    this.search();
+  }
+  search() {
+    if (this.searchTerm) {
+      this.subscriptionObject = this.onlineCourseworkService.search(this.searchTerm, this.page).subscribe(
+        x => { setTimeout(() => { this.result = x; this.postSearch(); }, 2000); }, err => { }, () => this.subscriptionObject.unsubscribe);
     }
-    }
+  }
   postSearch() {
-    console.log("result "+this.result+" totalrecords "+ this.result.totalNumberOfRecords+" data "+this.result.searchResults)
-    if (this.result.totalNumberOfRecords>0){
+    console.log("result " + this.result + " totalrecords " + this.result.totalNumberOfRecords + " data " + this.result.searchResults)
+    if (this.result.totalNumberOfRecords > 0) {
       this.resultsReturned = true;
-      this.results=this.result.searchResults; 
+      this.results = this.result.searchResults;
     }
-    else this.resultsReturned=false;   
+    else this.resultsReturned = false;
   }
- pageChanged(event:any):void{
- this.page=event.page;  
- this.search();
- }
+  pageChanged(event: any): void {
+    this.page = event.page;
+    this.search();
+  }
 }
